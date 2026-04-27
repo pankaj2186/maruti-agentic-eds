2f304f70d38d5d4ee3ac5a7dffda7239557d4058
pankaj2186 <pansingh@adobe.com>
2026-04-27 19:03:07 +0530

    Added custom function

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -17,6 +17,107 @@ import {validateKycDocumentNumber,
   sanitizeNumericField,
   initCibilFlow} from './scripts/form/personal-details';
 
+const marutiCarModels = [
+  {
+    id: 'NEW CELERIO',
+    name: 'New Celerio',
+    source: '/content/dam/forms-poc/maruti/celerio.png',
+    variants: ['LXi', 'VXi', 'ZXi', 'ZXi+', 'VXi CNG'],
+    colors: [
+      'Solid Fire Red',
+      'Speedy Blue',
+      'Silky Silver',
+      'Glistening Grey',
+      'Arctic White',
+      'Caffeine Brown',
+    ],
+  },
+  {
+    id: 'NEW ERTIGA',
+    name: 'New Ertiga',
+    source: '/content/dam/forms-poc/maruti/ertiga.png',
+    variants: ['LXi', 'VXi', 'ZXi', 'ZXi+', 'VXi CNG', 'ZXi CNG'],
+    colors: [
+      'Pearl Metallic Auburn Red',
+      'Magma Grey',
+      'Pearl Arctic White',
+      'Splendid Silver',
+      'Prime Oxford Blue',
+      'Dignity Brown',
+    ],
+  },
+  {
+    id: 'NEW SWIFT',
+    name: 'New Swift',
+    source: '/content/dam/forms-poc/maruti/swift.png',
+    variants: [
+      'LXi',
+      'VXi',
+      'VXi (O)',
+      'ZXi',
+      'ZXi+',
+      'VXi CNG / ZXi CNG', // or split: 'VXi CNG', 'ZXi CNG'
+    ],
+    colors: [
+      'Solid Fire Red',
+      'Pearl Arctic White',
+      'Metallic Silky Silver',
+      'Metallic Magma Grey',
+      'Pearl Midnight Blue',
+      'Dual-tone: Red/Black',
+      'Dual-tone: Blue/Black',
+    ],
+  },
+  {
+    id: 'NEW WAGONR',
+    name: 'New Wagon R',
+    source: '/content/dam/forms-poc/maruti/wagon-r.png',
+    variants: ['LXi 1.0', 'VXi 1.0', 'ZXi 1.2', 'ZXi+ 1.2', 'VXi CNG'],
+    colors: [
+      'Poolside Blue',
+      'Magma Grey',
+      'Silky Silver',
+      'Nutmeg Brown',
+      'Autumn Orange',
+      'Arctic White',
+    ],
+  },
+  {
+    id: 'S_PRESSO',
+    name: 'S-Presso',
+    source: '/content/dam/forms-poc/maruti/s-presso.png',
+    variants: ['Std', 'LXi', 'VXi', 'VXi+', 'VXi CNG'],
+    colors: [
+      'Solid Fire Red',
+      'Sizzle Orange',
+      'Starry Blue',
+      'Granite Grey',
+      'Silky Silver',
+      'Arctic White',
+    ],
+  },
+  {
+    id: 'VICTORIS',
+    name: 'Victoris (Arena SUV)', // or 'New Victoris' per your copy
+    source: '/content/dam/forms-poc/maruti/victoris.png',
+    variants: [
+      'LXi',
+      'VXi',
+      'ZXi',
+      'ZXi (O)',
+      'ZXi+',
+      'ZXi+ (O)',
+    ],
+    colors: [
+      'Nexa Blue / Deep Blue',
+      'Splendid Silver',
+      'Grandeur Grey',
+      'Arctic White',
+      'Opulent Red',
+      'Dual-tone: Black roof variants',
+    ],
+  },
+];
 
 /**
  * Set serial number for the current instance of a repeatable panel.
@@ -77,6 +178,48 @@ function days(endDate, startDate) {
   return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 }
 
+/**
+ * Populate variant dropdown based on selected car model from marutiCarModels.
+ * @name populateVariants
+ * @param {string} selectedModel  // value from the model dropdown (e.g. 'NEW CELERIO')
+ * @param {object} variantDropdown  // variant dropdown field reference
+ * @param {scope} globals
+ */
+function populateVariants(selectedModel, variantDropdown, globals) {
+  const model = marutiCarModels.find((m) => m.id === selectedModel);
+  if (!model) {
+    globals.functions.setProperty(variantDropdown, { enum: [], enumNames: [] });
+    globals.functions.setProperty(variantDropdown, { value: '' });
+    return;
+  }
+  const enumValues = model.variants;
+  const enumNames = model.variants;
+
+  globals.functions.setProperty(variantDropdown, { enum: enumValues, enumNames });
+  globals.functions.setProperty(variantDropdown, { value: enumValues[0] });
+}
+
+/**
+ * Populate color dropdown based on selected car model from marutiCarModels.
+ * @name populateColors
+ * @param {string} selectedModel  // value from the model dropdown (e.g. 'NEW CELERIO')
+ * @param {object} colorDropdown  // color dropdown field reference
+ * @param {scope} globals
+ */
+function populateColors(selectedModel, colorDropdown, globals) {
+  const model = marutiCarModels.find((m) => m.id === selectedModel);
+  if (!model) {
+    globals.functions.setProperty(colorDropdown, { enum: [], enumNames: [] });
+    globals.functions.setProperty(colorDropdown, { value: '' });
+    return;
+  }
+  const enumValues = model.colors;
+  const enumNames = model.colors;
+
+  globals.functions.setProperty(colorDropdown, { enum: enumValues, enumNames });
+  globals.functions.setProperty(colorDropdown, { value: enumValues[0] });
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export { 
   getFullName, 
@@ -100,5 +243,6 @@ export {
   sanitizeTextField,
   sanitizeNumericField,
   initCibilFlow,
-  setSerialNumber
+  populateVariants,
+  populateColors
 };
