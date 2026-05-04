345cede3dc7d6cbc9f59f10f7429fe519af0f656
pankaj2186 <pansingh@adobe.com>
2026-05-04 18:27:45 +0530

    Update employee-table.js

--- blocks/form/components/employee-table/employee-table.js
+++ blocks/form/components/employee-table/employee-table.js
@@ -90,8 +90,13 @@ export default function decorate(fieldDiv, fieldJson) {
 
   if (dataEndpoint) {
     tbody.innerHTML = '<tr><td colspan="4" class="loading">Loading...</td></tr>';
-    fetch(dataEndpoint)
-      .then((res) => {
+    fetch(dataEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+    }).then((res) => {
         if (!res.ok) throw new Error(res.statusText);
         return res.json();
       })
