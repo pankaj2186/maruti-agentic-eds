6a50224be21673a6f9bbc803dd5dbb89273bcaa4
pankaj2186 <pansingh@adobe.com>
2026-05-04 15:17:47 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -20,7 +20,7 @@ import {validateKycDocumentNumber,
   import {populateVariants,
     populateColors,
     mobileBrowser,
-    setSerialNumber, prefillFromQueryParams, setEnquiryNumber, fetchFromQueryParams, fetchJsonProperty} from './scripts/form/dealer-functions.js';
+    setSerialNumber, prefillFromQueryParams, setEnquiryNumber, fetchFromQueryParams, fetchJsonProperty, maskAadhaar} from './scripts/form/dealer-functions.js';
 
 /**
  * Get Full Name
@@ -96,5 +96,6 @@ export {
   prefillFromQueryParams,
   setEnquiryNumber,
   fetchFromQueryParams,
-  fetchJsonProperty
+  fetchJsonProperty,
+  maskAadhaar
 };
